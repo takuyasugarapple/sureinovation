4c6f1deb50cafa462b0240546800fe9a36e7f4c8
takuyasugarapple <38278218+takuyasugarapple@users.noreply.github.com>
2018-09-25 18:43:14 +0900

    Update jq.js

--- js/jq.js
+++ js/jq.js
@@ -1,12 +1,9 @@
 $(function () {
 
     //.accordion1の中のp要素がクリックされたら
-    $('.accordion1 p').click(function () {
-
-        //クリックされた.accordion1の中のp要素に隣接するul要素が開いたり閉じたりする。
-        $(this).next('ul').slideToggle();
-
-    });
+    $('.accordion1 .li-question').click(function () {
+        $(this).next('.li-answer').slideToggle();
+});
     
     $('a[href^="#"]').click(function() {
     var href = $(this).attr("href");
